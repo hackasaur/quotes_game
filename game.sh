d1f77686cc5700158f631d5ad07167b3cc7548e8
Manik Raina <manikraina1096@gmail.com>
2019-08-14 19:29:16 +0530

    added color, score and changed the title art

--- game.sh
+++ game.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 clear
+
+#colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+RESET='\033[0m'
+
 #title art
 file1="./art/titleart"
 while IFS= read -r line
@@ -10,11 +16,13 @@ done <"$file1"
 sleep 1
 printf "\n\n"
 
+score=0
+#body of the game starts here:
 while true; do
 
 #to select the file name to fetch quotes from
 echo "list of available authors:"
-ls authors
+ls -a authors
 
 echo choose an author:
 read file
@@ -33,7 +41,8 @@ printf "\n\n"
 
 #quiz to test if player remembers each quote one by one
 for (( i=0; i<n; i++));
-do
+do	
+	echo "Score:$score"
 	echo '"'${quotes[i]}'"'
 	read -s -p "remember the quote and press enter"
 	clear
@@ -41,14 +50,16 @@ do
 	read string
 	if [ "$string" == "${quotes[i]}" ]
 	then
-		echo "correct!"
+		echo -e "${GREEN}correct!${RESET}"
+		((score++))
 	else 
-		echo "wrong!"
+		echo -e "${RED}wrong!${RESET}"
 		echo "correct answer is: ${quotes[i]}"
 	fi
 	read -p "press enter"
 	clear
 done
+echo "your score is:$score"
 
 done
 
